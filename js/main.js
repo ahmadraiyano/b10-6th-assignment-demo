12380c48bcbba3f0df866ca62cf1e48118b47881
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2024-10-14 21:08:01 +0600

    fetch button done

--- js/main.js
+++ js/main.js
@@ -3,10 +3,34 @@ const loadCategories = () => {
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error))
-}
+};
 
-const displayCategories = (data) => {
-    console.log(data);
-}
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories");
+
+
+    categories.forEach((item) => {
+        console.log(item);
+
+        const img = document.createElement("img");
+        const button = document.createElement("button");
+        const a = document.createElement("a");
+        img.src = item.category_icon;
+        img.classList = "w-6";
+        a.innerText = item.category;
+        button.classList = "flex sm:gap-1 border rounded-xl p-2 sm:px-8";
+
+        button.append(img);
+        button.append(a);
+
+        categoryContainer.append(button);
+    });
+};
 
 loadCategories();
+
+// {
+//     "id": 1,
+//     "category": "Cat",
+//     "category_icon": "https://i.ibb.co.com/N7dM2K1/cat.png"
+// }
